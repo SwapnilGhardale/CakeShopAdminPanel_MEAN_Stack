f4497212c09d7478d98157c62dcb6426ea3623ba
Swapnil Ghardale <ghardaleswapnil@gmail.com>
2020-06-29 20:53:09 +0530

    intialized the components required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainscreenComponent } from './mainscreen/mainscreen.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainscreenComponent,
+    CustomersComponent,
+    OrdersComponent,
+    ProductsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
