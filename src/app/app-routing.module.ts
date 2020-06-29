c5a83535c266fce6c7a2276e60dcebed1d9c4cae
Swapnil Ghardale <ghardaleswapnil@gmail.com>
2020-06-30 02:43:16 +0530

    added header and added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainscreenComponent } from './mainscreen/mainscreen.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CategoryComponent } from './category/category.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+{path:'main',component:MainscreenComponent},
+{path:'orders',component:OrdersComponent},
+{path:'product',component:ProductsComponent},
+{path:'customers',component:CustomersComponent},
+{path:'category',component:CategoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
